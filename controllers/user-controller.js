828bc85c9f1f694b338d9a482a89fcaebf9b1e64
soo5717 <koo5717@naver.com>
2021-01-09 13:51:03 +0900

    fix(Users): delete user controller and user service code

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,12 +5,5 @@ const sc = require('../modules/status-code');
 const jwt = require('../modules/jwt');
 
 module.exports = {
-    signup: async (req, res) => {
-        try {
-           console.log('회원가입 컨트롤러'); 
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
-    }
+
 }
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ var router = express.Router();
 const { checkToken } = require('../middleware/auth');
 const userController = require('../controllers/user-controller');
 
-// [POST] 회원가입 
-router.post('/signup', userController.signup);
+
 
 module.exports = router;
