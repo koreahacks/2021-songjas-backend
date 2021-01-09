684c25777d87489d2663eeaaa65c5d6bc750eecd
soo5717 <koo5717@naver.com>
2021-01-09 20:59:59 +0900

    feat(Users): [POST] uploadProjectImage, createProject

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const upload = require('../modules/multer');
 const { checkToken } = require('../middleware/auth');
 const projectController = require('../controllers/project-controller');
 
-
+// [POST] 팀모 이미지 등록
+router.post('/images', checkToken, upload.single('img'), projectController.uploadProjectImage);
+// [POST] 팀모(모집글) 등록
+router.post('/', checkToken, projectController.createProject);
 
 module.exports = router;
