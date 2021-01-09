98f9993d1b9bd7632f4305e7d25adde90635b246
expresshighway <03wow22@gmail.com>
2021-01-09 17:10:40 +0900

    feat(members): [POST] members

--- controllers/member-controller.js
+++ controllers/member-controller.js
@@ -2,7 +2,24 @@ const memberService = require('../services/member-service');
 const rb = require('../modules/response-body');
 const rm = require('../modules/response-message');
 const sc = require('../modules/status-code');
+const projectService = require('../services/project-service');
 
 module.exports = {
-    
+    createMember: async (req, res) => {
+        const { title, type, field, content, link, open, MemberPositions, MemberActivities } = req.body;
+        if(!title || !type || !field || !content || link === undefined || open === undefined || 
+            !MemberPositions || !MemberActivities) { //링크는 allowNull
+            return res.status(sc.BAD_REQUEST).send(rb.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
+        }
+        try {
+            let member = req.body;
+            member.UserId = req.decoded; //Json Key, Value 추가
+
+            await memberService.createMember(member);
+            return res.status(sc.CREATED).send(rb.success(sc.CREATED, rm.MEMBER_CREATE_SUCCESS));            
+        } catch (error) {
+            console.error(error);
+            return res.status(sc.INTERNAL_SERVER_ERROR).send(rb.fail(sc.INTERNAL_SERVER_ERROR, rm.MEMBER_CREATE_FAIL));
+        }
+    },
 }
--- routes/members.js
+++ routes/members.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const { checkToken } = require('../middleware/auth');
 const memberController = require('../controllers/member-controller');
 
-
+// [POST] 팀글 생성
+router.post('/', checkToken, memberController.createMember);
 
 module.exports = router;
