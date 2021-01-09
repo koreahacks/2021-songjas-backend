fcaf9c1e633fcfb8f7e3d0d4795adb6e8efc1bd8
soo5717 <koo5717@naver.com>
2021-01-09 20:59:59 +0900

    feat(Users): [POST] uploadProjectImage, createProject

--- controllers/project-controller.js
+++ controllers/project-controller.js
@@ -4,5 +4,44 @@ const rm = require('../modules/response-message');
 const sc = require('../modules/status-code');
 
 module.exports = {
-    
+    uploadProjectImage: async (req, res) => {
+        if(!req.file) {
+            return res.status(sc.BAD_REQUEST).send(rb.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
+        }
+        try {
+            console.log(req.file);
+            const result = { img: req.file.location };
+            return res.status(sc.CREATED).send(rb.successData(sc.CREATED, rm.PROJECT_IMAGE_UPLOAD_SUCCESS, result));
+        } catch (error) {
+            console.error(error);
+            return res.status(sc.INTERNAL_SERVER_ERROR).send(rb.fail(sc.INTERNAL_SERVER_ERROR, rm.PROJECT_IMAGE_UPLOAD_FAIL));
+        }
+    },
+    createProject: async (req, res) => {
+        const { 
+            img, room, title, type, field, startDate, endDate, content, link,
+            morning, night, dawn, plan, cramming, leader, follower, challenge, realistic, 
+            largeAddress, smallAddress, limitUniv, ProjectMembers, ProjectPositions 
+        } = req.body;
+        if(img === undefined || !room || !title || !type || !field || !startDate || !endDate || !content || link === undefined ||
+            morning === undefined || night === undefined || dawn === undefined || 
+            plan === undefined || cramming === undefined || leader === undefined || 
+            follower === undefined || challenge === undefined || realistic === undefined ||
+            !largeAddress || !smallAddress || limitUniv === undefined || !ProjectMembers || !ProjectPositions) {
+            console.log(req.body);
+            return res.status(sc.BAD_REQUEST).send(rb.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
+        }
+        try {
+            let project = req.body;
+            project.UserId = req.decoded;
+            console.log(project);
+            
+            console.log(req.body);
+            await projectService.createProject(project);
+            return res.status(sc.CREATED).send(rb.success(sc.CREATED, rm.PROJECT_CREATE_SUCCESS));
+        } catch (error) {
+            console.error(error);
+            return res.status(sc.INTERNAL_SERVER_ERROR).send(rb.fail(sc.INTERNAL_SERVER_ERROR, rm.PROJECT_CREATE_FAIL));
+        }
+    },
 }
--- services/project-service.js
+++ services/project-service.js
@@ -1,5 +1,20 @@
-// 주석 지우고 모델 선언 추가
+const { Project, ProjectPosition, ProjectMember, User } = require('../models');
 
 module.exports = {
-
+    createProject: async (project) => {
+        // 전체, 자대 boolean에서 문자열로 변경
+        const user = await User.findByPk(project.UserId);
+        project.limitUniv = user.univ;
+        // 연관 테이블과 함께 생성
+        await Project.create(project, {
+            include: [
+                {
+                    model: ProjectMember
+                },
+                {
+                    model: ProjectPosition
+                }
+            ]
+        });
+    },
 }
