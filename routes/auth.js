6c9b066530eba0f8bffe35a5957d5ced6873b8c2
soo5717 <koo5717@naver.com>
2021-01-09 19:04:08 +0900

    feat(Auth): [GET] autoSignIn

--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,7 @@ const authController = require('../controllers/auth-controller');
 // [POST] 이메일 인증 
 router.post('/email', authController.email);
 
+// [GET] 자동 로그인 : 토큰 검증
+router.get('/', checkToken, authController.autoSignIn);
+
 module.exports = router;
